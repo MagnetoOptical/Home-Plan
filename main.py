25f054545d1b553570575564286928c9ff110e6e
Sean Seago <sean.seago@projectceladon.org>
2020-03-16 09:33:41 -0500

    Just getting started

---
+++ main.py
@@ -0,0 +1,9 @@
+# TODO:
+# 1. Connect to bank account info store in encrypted file;
+#   thread monitors transactions
+# 2. other stuff...may require additional threads
+
+
+import andromedadb
+
+# See if a database exists...
